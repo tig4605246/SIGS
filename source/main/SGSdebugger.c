0a53f3f74d3d4cfdb5baaf1b786bde3b6b37a9a5
Kelier <tig4605246@gmail.com>
2017-03-10 16:11:06 +0800

    sort the path and files

---
+++ source/main/SGSdebugger.c
@@ -0,0 +1,22 @@
+/*
+    Name: Xu Xi-Ping
+    Date: March 1,2017
+    Last Update: March 8,2017
+    Program statement: 
+        This is a debug program. It'll do the following task:
+            1. check correctness of the configure file
+            2. show the content of shared memory
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+    
+
+
+}
