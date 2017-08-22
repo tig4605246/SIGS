26d34a9a8f1190a15feed87cd718ffbb2510f977
Kelier <tig4605246@gmail.com>
2017-08-22 13:36:18 +0800

    revise path for startSGS.sh

--- startSGS.sh
+++ startSGS.sh
@@ -2,12 +2,12 @@
 
 #set up directories and start up the SEG 
 
-SGSPATH=/home/SGS/SGS/
+SGSPATH=/home/ntust/SIGS/
 
 case "$1" in
    start)
 
- 	echo -n "Starting SGSmaster: \n"
+ 	echo -n "Starting cpm70 and aemdra agents: \n"
         cd ${SGSPATH}bin/
 
         ./cpm70_agent &
@@ -18,7 +18,7 @@ case "$1" in
 
    stop)
 
- 	echo -n "Stoping SGSmaster: \n"
+ 	echo -n "Stoping cpm70 and aemdra agents: \n"
         cpm70_PID=`cat ${SGSPATH}bin/pid/cpm70_agent.pid`
         aemdra_PID=`cat ${SGSPATH}bin/pid/aemdra_agent.pid`
         kill -15 ${cpm70_PID} ${aemdra_PID}
