04a425f9b71a043150ac8d9bd9625eb8ad31de91
Kelier <tig4605246@gmail.com>
2017-10-30 02:34:12 +0800

    update aemdra: try to fix minus number (stof: declare params at the starting point)

--- source/collector/aemdra_agent_standalone.c
+++ source/collector/aemdra_agent_standalone.c
@@ -843,19 +843,21 @@ int CheckDuplicateByTime(char *ID, char *time, char *blockId)
 float stof(const char* s)
 {
     float rez = 0, fact = 1;
+    int point_seen = 0;
+    int d;
     if (*s == '-')
     {
       s++;
       fact = -1;
     }
-    for (int point_seen = 0; *s; s++)
+    for (point_seen = 0; *s; s++)
     {
       if (*s == '.')
       {
         point_seen = 1; 
         continue;
       }
-      int d = *s - '0';
+      d = *s - '0';
       if (d >= 0 && d <= 9)
       {
         if (point_seen) fact /= 10.0f;
